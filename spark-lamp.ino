38edb39e07ae8eac2f3f27fd406dc4b61d8d65c8
Dillon <ee.tinkerer@gmail.com>
2014-12-18 09:37:49 -0500

    use clickButton instead of interrupts

--- spark-lamp.ino
+++ spark-lamp.ino
@@ -28,16 +28,31 @@ RCSwitch mySwitch = RCSwitch();
 #define SARA_BIT 0b10   // bit 1 is the state of Sara's lamp
 int lampState = 0b00;   // stores the state of both lamps
 
-void setup() {
-    // set up the 3 switch pins as inputs and pulled down
-    pinMode(dillonLamp, INPUT_PULLDOWN);
+// This #include statement was automatically added by the Spark IDE.
+#include "clickButton/clickButton.h"
+
+// the Button
+//const int buttonPin1 = 4;
+ClickButton dillonButton(dillonLamp, LOW, CLICKBTN_PULLUP);
+
+// Button results 
+int dillonClicks = 0;
+
+
+void setup()
+{
+    Serial.begin(9600);
+    //pinMode(D4, INPUT_PULLUP);
+
+    // set up the 3 switch pins as inputs
+    pinMode(dillonLamp, INPUT_PULLUP);
     pinMode(saraLamp, INPUT_PULLDOWN);
     pinMode(lightSwitch, INPUT_PULLDOWN);
-    
+
     // if the pin changes value, go to its corresponding interrupt
-    attachInterrupt(dillonLamp,   toggleDillon, RISING);
-    attachInterrupt(saraLamp,     toggleSara,   RISING);
-    attachInterrupt(lightSwitch,  switchLamps,  CHANGE);
+    //attachInterrupt(dillonLamp,   toggleDillon, RISING);
+    //attachInterrupt(saraLamp,     toggleSara,   RISING);
+    //attachInterrupt(lightSwitch,  switchLamps,  CHANGE);
 
     // setup the transmitter on pin D0
     mySwitch.enableTransmit(TRANSMITTER);
@@ -46,14 +61,48 @@ void setup() {
 
     // create function(s) so app can change states
     Spark.function("switch", webSwitch);
-    
+
     // make variable available to GET
     Spark.variable("state", &lampState, INT);
+
+  // Setup button timers (all in milliseconds / ms)
+  // (These are default if not set, but changeable for convenience)
+  dillonButton.debounceTime   = 20;   // Debounce timer in ms
+  dillonButton.multiclickTime = 250;  // Time limit for multi clicks
+  dillonButton.longClickTime  = 1000; // time until "held-down clicks" register
 }
 
-void loop() {
-    
+
+void loop()
+{
+  // Update button state
+  dillonButton.Update();
+
+  // Save click codes in dillonClicks, as click codes are reset at next Update()
+  // is this line even needed?
+  // can't I just directly use dillonButton.clicks ?
+  // maybe I can wrap the 3 dillonClicks calls into this function?
+  if (dillonButton.clicks != 0) dillonClicks = dillonButton.clicks;
+  
+  if(dillonClicks == 1){
+      Serial.println("SINGLE click");
+      toggleDillon();
+  }
+
+  if(dillonClicks == 2){
+    Serial.println("DOUBLE click");
+    toggleSara();
+  } 
+
+if(dillonClicks == -1){
+      Serial.println("SINGLE LONG click");
+      matchToggle("DILLON");
+  }
+  
+  dillonClicks = 0;
+  delay(5);
 }
+
 
 // Exposed function to turn lamps on, off, or toggle individually
 int webSwitch(String state){
@@ -89,8 +138,8 @@ void toggleDillon(){
         // turn off Dillon's lamp
         mySwitch.send(DILLON_OFF, BIT_LENGTH);
     }
-    else{
-        // turn on Dillon's lamp
+    if (newState == 1){
+        //turn on Dillon's lamp
         mySwitch.send(DILLON_ON, BIT_LENGTH);
     }
 }
@@ -111,6 +160,34 @@ void toggleSara(){
     }
 }
 
+// toggle the cooresponding button's lamp
+// and then match the other lamp to that lamp's current state
+void matchToggle(String button){
+    if (button == "DILLON"){
+        toggleDillon();
+        int dillonState = lampState & DILLON_BIT;
+        int saraState = (lampState & SARA_BIT) >> 1;
+        //Serial.print(lampState);
+        //Serial.print(dillonState);
+        //Serial.println(saraState);
+        if (dillonState != saraState){
+            toggleSara();
+        }  
+    }
+    else if (button == "SARA"){
+        toggleSara();
+        int dillonState = lampState & DILLON_BIT;
+        int saraState = (lampState & SARA_BIT) >> 1;
+        //Serial.print(lampState);
+        //Serial.print(dillonState);
+        //Serial.println(saraState);
+        if (dillonState != saraState){
+            toggleDillon();
+        }   
+    }
+    
+}
+
 void switchLamps(){
     if (digitalRead(lightSwitch) == LOW){
         // turn both lamps off
@@ -125,4 +202,3 @@ void switchLamps(){
         lampState = 0b11;
     }
 }
-
